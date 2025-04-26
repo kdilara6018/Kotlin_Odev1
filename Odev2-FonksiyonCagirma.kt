788758b1d09f132f1ef00f93f528e70228c5b275
kdilara6018 <kdilara6018@gmail.com>
2025-04-26 17:31:00 +0300

    Add files via upload

---
+++ Odev2-FonksiyonCagirma.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin1.Odevler
+
+fun main()
+{
+    val f=Odev2_FonksiyonOlusturma()
+    f.fonksiyon1(4)
+    f.fonksiyon2(30)
+    f.fonksiyon3(40)
+    f.fonksiyon4(170)
+    f.fonksiyon5(5,6,-1,3,)
+    f.fonksiyon6(5)
+    val fonksiyon7sonuc=f.fonksiyon7("dilarA KARATas")
+    println(fonksiyon7sonuc)
+}
