b6f7491313d850bcde5200cd53362cc83a0c0086
kdilara6018 <kdilara6018@gmail.com>
2025-04-26 14:41:49 +0300

    Add files via upload

---
+++ Odev1-DegiskenOlusturma.kt
@@ -0,0 +1,102 @@
+package com.example.kotlin1.Odevler
+
+fun main()
+{
+    // val = değiştirilemez değişken oluşturmak için
+    // var = sonraki satırlarda değiştilebilir , esnek yapı oluşturmak için
+    val sehir ="Istanbul"
+    val ulke = "Turkiye"
+    val telefon = 5303005050
+    val posta_kodu = 34000
+    val e_mail = "kdilara6018@gmail.com"
+    val meslek = "yazilim muhendisi"
+    val stok_miktari = 2000
+    val musteri_adi = "Dilara Karatas"
+    val bakiye = 1000
+    val dogum_gunu = "29 Aralik 2003"
+    val maas = 1000
+    val medeni_durum = "bekar"
+    val urun_yorum = "urun kullanisli 5 yildiz"
+    var odeme_tairihi = "28 Nisan"
+    var odeme = "50 tl"
+    var siparis_adeti = 200
+    var araba_modeli = "Volvo"
+    var kitap_adi = "Rezonans Kanunu"
+    var yayinlanma_tarihi = "30 Mart 2025"
+    var indirim_miktari = "%50"
+    var oda_sayisi = 3
+    var enlem = 50
+    var boylam = 30
+    var urun_adi = "mobil application"
+    var yemek_fiyati = 250
+    val marka = "Lalabag"
+    val muzik_adi = "Sezen Aksu - Farkındayım"
+    val video_suresi = 3.58
+    val urun_puani = 4.5
+    val resim_adi = "x.png"
+    val dosya_formati = "y.html"
+    val renk = "yesil"
+    val renk_kodu = 1234
+    val telefon_modeli = "iphone 11"
+    val ekran_boyutu = "16 x 11 inc"
+    val agirlik = 55.5
+    var ulusal_gun = "23 Nisan Çocuk Bayramı"
+    var tatil_gun = "23 Nisan"
+    var rezervasyon_tarihi = "23 Nisan - 28 Nisan 2025"
+    var sokak_adi = "Huner sk"
+    var otobus_hatti = "50 D"
+    var kalan_dakika = 44
+    var takip_kodu = 1234
+    var kupon_suresi = "30 dk"
+    var kupon_kodu = "1234Dilara50"
+    var fatura_adresi = "Eren Kırtasiye x mah. y sk."
+
+    //println() = değişkeni yazdırmak ve her seferinde alt satıra geçmesi için kullanıcağımız yapı
+
+    println(sehir)
+    println(ulke)
+    println(telefon)
+    println(posta_kodu)
+    println(e_mail)
+    println(meslek)
+    println(stok_miktari)
+    println(musteri_adi)
+    println(bakiye)
+    println(dogum_gunu)
+    println(maas)
+    println(medeni_durum)
+    println(urun_yorum)
+    println(odeme_tairihi)
+    println(odeme)
+    println(siparis_adeti)
+    println(araba_modeli)
+    println(kitap_adi)
+    println(yayinlanma_tarihi)
+    println(indirim_miktari)
+    println(oda_sayisi)
+    println(enlem)
+    println(boylam)
+    println(urun_adi)
+    println(yemek_fiyati)
+    println(marka)
+    println(muzik_adi)
+    println(video_suresi)
+    println(urun_puani)
+    println(resim_adi)
+    println(dosya_formati)
+    println(renk)
+    println(renk_kodu)
+    println(telefon_modeli)
+    println(ekran_boyutu)
+    println(agirlik)
+    println(ulusal_gun)
+    println(tatil_gun)
+    println(rezervasyon_tarihi)
+    println(sokak_adi)
+    println(otobus_hatti)
+    println(kalan_dakika)
+    println(takip_kodu)
+    println(kupon_suresi)
+    println(kupon_kodu)
+    println(fatura_adresi)
+}
